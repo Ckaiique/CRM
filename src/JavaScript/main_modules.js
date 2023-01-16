328f9efda1293c9b4b88f91d8d410f0149df0118
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2023-01-15 22:04:13 -0300

    Criação da Page Home
    criando a page Home onde vai conter funil de vendas crm

---
+++ src/JavaScript/main_modules.js
@@ -0,0 +1,35 @@
+
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById('open').innerHTML = "CRM";
+ 
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+  document.getElementById('open').innerHTML  =  '&#9776; CRM';
+ 
+}
+
+
+function Links(link){
+  document.getElementById('title').innerHTML = link;
+
+  var url = 'Route/'+link+'_modules.html';
+  var xml = new XMLHttpRequest();
+  
+  xml.onreadystatechange = function(){
+    console.log(link)
+    if(xml.readyState == 4 && xml.status == 200){
+      document.getElementById('element').innerHTML = xml.responseText;
+
+    }
+  }
+  xml.open("GET",url,true);
+  xml.send()
+}
