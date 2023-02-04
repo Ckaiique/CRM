b40d881f22bdac183eb523049f4301a92e9913c4
kaique correia chagas <80991704+Ckaiique@users.noreply.github.com>
2023-02-04 13:34:41 -0300

    implementaçoes
    terminado a parte do cadastro do cliente  e agora  colocando datatable

--- src/JavaScript/Clients_modules.js
+++ src/JavaScript/Clients_modules.js
@@ -1,21 +1,26 @@
-function saveCadastro(){
-  for (let index = 0; index < 4; index++) {
-  var el = document.getElementById('modalCadastro');
-   var imputs = el.children[index].getElementsByClassName('form-control')[index];
-   console.log(imputs.value )
-  if(typeof imputs != 'undefined' && !imputs.value   ) {
-    
-    return Swal.fire({
-        icon: 'warning',
-        title: 'Aviso',
-        text: `O campo "${imputs.id}" esta vazio `
-      })
-  } 
-    
-  }
+function saveCadastro() {
+
+    var campo_obrigatorios =
+        [
+            document.getElementById('Cliente').value,
+            document.getElementById('Empresa').value,
+            document.getElementById('Contato').value,
+            document.getElementById('date').value
+        ];
+    var nm_campos = ['Cliente', 'Empresa', 'Contato','Dt. Criação']  ;  
+
+ for (let index = 0; index < campo_obrigatorios.length; index++) {
+  
+    if (!campo_obrigatorios[index]) {
+        return Swal.fire({
+            icon: 'warning',
+            title: 'Aviso',
+            html: `O campo <b>${nm_campos[index]}</b> esta vazio`
+        });
+    }
+ }
+
 }
-
-
 
 
 
@@ -34,12 +39,12 @@ function consultaCep() {
         }
 
 
-        if (!validate_cep(cep)){
-          return Swal.fire({
+        if (!validate_cep(cep)) {
+            return Swal.fire({
                 icon: 'warning',
                 title: 'Aviso',
                 text: `CEP "${cep}" Formato invalido `
-              })
+            })
         }
 
         var url = 'https://viacep.com.br/ws/' + cep.trim() + '/json/';
@@ -49,20 +54,19 @@ function consultaCep() {
             if (xml.readyState == 4 && xml.status == 200) {
                 var dados = JSON.parse(xml.response);
 
-                if(!dados.erro){
+                if (!dados.erro) {
                     document.getElementById('CEP').value = dados.cep;
                     document.getElementById('Endereco').value = dados.logradouro;
                     document.getElementById('Bairro').value = dados.bairro;
                     document.getElementById('Cidade').value = dados.localidade;
                     document.getElementById('Estado').value = dados.uf;
-                }else{
+                } else {
                     return Swal.fire({
                         icon: 'warning',
                         title: 'Aviso',
                         text: `Não foi posível encontrar o CEP "${validate_cep(cep)}" `
-                      })
+                    })
                 }
-
             }
         }
         xml.open("GET", url, true);
@@ -73,3 +77,19 @@ function consultaCep() {
 
 
 
+function comboBox(params) {
+    document.getElementById('title_modal_combBox').innerHTML = params[0].toUpperCase() + params.substr(1);
+}
+
+function saveCategoriasComboBox(){
+   var tipo = document.getElementById('title_modal_combBox').innerHTML;
+   
+ 
+
+   if ( tipo == "Categoria" ) {
+    }
+
+    if( tipo == "Situacao"){
+    }
+   
+}
